08b8c9da107fa4c022b2804a09c9942b8cb5df6e
mwezn <m.w.g.nelson@gmail.com>
2025-08-22 22:19:58 +0100

    added monitor + keyboard

--- js/keyboard.js
+++ js/keyboard.js
@@ -17,7 +17,7 @@ const audioLoader = new THREE.AudioLoader();
 audioLoader.load( '../keypress.mp3', function( buffer ) {
 	sound.setBuffer( buffer );
 	//sound.setLoop(true);
-	sound.setVolume(0.5);
+	sound.setVolume(1);
 	
 });
 
@@ -44,7 +44,7 @@ document.body.appendChild(cssRenderer.domElement);
 
 
 
-    camera.position.set(10,3,0)
+    camera.position.set(0,3,10)
     
     var light = new THREE.AmbientLight(0xffffff, 5);
     scene.add(light,gridHelper);
@@ -71,11 +71,13 @@ document.body.appendChild(cssRenderer.domElement);
 
     var loader = new THREE.GLTFLoader()//.setPath('../monitor/');    
     // Load a glTF resource
-   /* loader.load('../rgbkeyboard.glb',function ( gltf ) {
+   loader.load('../monitor/OldMonitor.gltf',function ( gltf ) {
 
         monitor=gltf.scene;
+        monitor.scale.set(3,3,3)
+        monitor.position.y=3;
         scene.add(monitor);
-        /*gltf.scene.traverse(function(child){
+        gltf.scene.traverse(function(child){
             console.log(child.name);
         })
 
@@ -84,14 +86,14 @@ document.body.appendChild(cssRenderer.domElement);
         
     }, undefined, function ( error ) {
         console.error( error );
-    } ); */
+    } ); 
 
     loader.load('../RGB/edit.glb',function ( gltf ) {
         keypress=gltf.animations
         keyboard=gltf.scene;
-        keyboard.position.x=0;
-        keyboard.position.set(0,0,0)
-        keyboard.scale.set(10,10,10)
+        keyboard.rotation.x=Math.PI/4
+        keyboard.position.set(0,0,5)
+        keyboard.scale.set(8,8,8)
         scene.add(keyboard);
 
      /*  mixer = new THREE.AnimationMixer(keyboard);
@@ -108,6 +110,79 @@ document.body.appendChild(cssRenderer.domElement);
     } );
 
     document.addEventListener('keydown', (event) => {
+    if (!keyboard || !keypress) return;
+
+    sound.play();
+    anim = new THREE.AnimationMixer(keyboard);
+
+    const playAnimationPair = (index, time) => {
+        const action1 = anim.clipAction(keypress[index]);
+        const action2 = anim.clipAction(keypress[index + 1]);
+        action1.time = time;
+        action2.time = time;
+        action1.setLoop(THREE.LoopOnce);
+        action2.setLoop(THREE.LoopOnce);
+        action1.play();
+        action2.play();
+    };
+
+    const keyAnimationMap = {
+        b: { index: 0, time: 1.0 },
+        s: { index: 2, time: 3 },
+        d: { index: 4, time: 3.4 },
+        f: { index: 6, time: 3.8 },
+        g: { index: 8, time: 4.2 },
+        h: { index: 10, time: 4.6 },
+        i: { index: 16, time: 5.0 },
+        m: { index: 18, time: 5.4},
+        k: { index: 20, time: 5.8},
+        l: { index: 22, time: 3.2 },
+        n: { index: 26, time: 3.6 },
+        o: { index: 28, time: 3.8 },
+        p: { index: 30, time: 4.0 },
+        q: { index: 32, time: 4.2 },
+        r: { index: 34, time: 4.4 },
+        a: { index: 36, time: 4.6 },
+        t: { index: 38, time: 4.8 },
+        u: { index: 40, time: 5.0 },
+        v: { index: 42, time: 5.2 },
+        w: { index: 44, time: 5.4 },
+        x: { index: 46, time: 5.6 },
+        y: { index: 48, time: 5.8 },
+        z: { index: 50, time: 6.0 },
+    };
+
+    const key = event.key.toLowerCase();
+
+    if (key in keyAnimationMap) {
+        const { index, time } = keyAnimationMap[key];
+        playAnimationPair(index, time);
+        return;
+    }
+
+    // Handle special keys separately
+    switch (key) {
+        case 'arrowup':
+            keyboard.position.z -= 0.1;
+            console.log('UP PRESSED');
+            break;
+        case 'arrowdown':
+            // keyboard.position.z += 0.1;
+            break;
+        case 'arrowleft':
+            playAnimationPair(0, 1.8); // Customize index/time as needed
+            break;
+        case 'arrowright':
+            keyboard.rotation.y -= 0.1;
+            break;
+        case ' ':
+            // Spacebar logic
+            break;
+    }
+});
+
+
+    /*document.addEventListener('keydown', (event) => {
 
         sound.play();
         anim = new THREE.AnimationMixer(keyboard);
@@ -206,7 +281,7 @@ document.body.appendChild(cssRenderer.domElement);
 
            
     }
-});
+});*/
 
 /*
  
@@ -228,6 +303,31 @@ document.body.appendChild(cssRenderer.domElement);
 
     clock = new THREE.Clock();
 
+    function Element2( path, x, y, z, ry,css) {
+
+            const div = document.createElement( 'div' );
+            const iframe = document.createElement('iframe');
+            iframe.style.border = '0px';
+            iframe.src = path
+            //if(loop) iframe.loop='true'
+            //if (autoplay) iframe.allow='autoplay'
+            //css=='monitor'?div.className='css3d-object':div.className='oldtv'
+            div.className=css;
+            
+            div.appendChild(iframe)
+            //container.appendChild(div)
+
+            const object = new CSS3DObject( div );
+            object.position.set( x, y, z );
+            object.rotation.y = ry;
+            object.scale.set(0.01, 0.01, 0.01);
+            return object;
+        }
+    
+    const scope= new Element2('https://www.wikipedia.org/',0,3,0,0,'monitor')
+
+    scene.add(scope)
+
 
     
     function animate() {
@@ -235,6 +335,7 @@ document.body.appendChild(cssRenderer.domElement);
         var delta = clock.getDelta();
         if ( mixer ) mixer.update( delta );
         if (anim)   anim.update(delta)
+
          
         requestAnimationFrame(animate);
         webGLRenderer.render(scene, camera);
